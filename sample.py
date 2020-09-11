b36e26133f9f51243a5dff1a095f4ddf586d4990
normanazares <azares.norman410@gmail.com>
2020-09-11 21:17:14 +0800

    changes

--- sample.py
+++ sample.py
@@ -1,3 +1,13 @@
-data = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
-pos = data.find('.')
-print(data[pos:pos+3])
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+count = 0
+s = 0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:") :
+        continue
+    pos = line.find('0')
+    s += float(line[pos:pos+6])
+    count += 1
+    average = s / count
+print("Average spam confidence:", average)
